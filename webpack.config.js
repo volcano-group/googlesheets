c03a8f58c0edc78ab22f0f91c7b03f82405a8df1
Alessandro Locatelli <alessandro.locatelli95@gmail.com>
2017-05-06 16:14:22 +0200

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,41 @@
+var webpack = require('webpack');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: './dist/bundle.js'
-  }
+    target: 'node',
+    entry: './src/index.js',
+    output: {
+        filename: './dist/bundle.js'
+    },
+    plugins: [
+      new webpack.BannerPlugin({banner: 'require("source-map-support").install();',
+                                raw: true, entryOnly: false })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+    externals: nodeModules,
+    devtool: 'sourcemap'
 };
