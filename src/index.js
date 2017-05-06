acf13c5cb33b6dcf3e3606defa0fd83a3397bb4e
Alessandro Locatelli <alessandro.locatelli95@gmail.com>
2017-05-06 17:40:35 +0200

    funzione a caso in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+function saluta(name) {
+    do if (1==1) {
+        let nome = name;
+        console.log("ciao ", nome);
+    } else {
+        console.log("ciao");
+    }
+}
+
+saluta("alberto");
