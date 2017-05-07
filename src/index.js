f84667ad96c2a5d459c2a27bc82759497e8837b5
Alessandro Locatelli <alessandro.locatelli95@gmail.com>
2017-05-07 15:04:43 +0200

    nulla

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-function saluta(name) {
-    do if (1==1) {
-        let nome = name;
-        console.log("ciao ", nome);
-    } else {
-        console.log("ciao");
-    }
-}
-
-saluta("alberto");
+console.log("Hello world");
