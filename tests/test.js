70cba8d4ab40027a3a3acd57c1adbeec6e90f651
Alessandro Locatelli <alessandro.locatelli95@gmail.com>
2017-05-07 17:25:54 +0200

    some fix

--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,7 @@ let app = require('../src/index.js');
 
 describe('Salutare', () => {
     it('it should say Hello', (done) => {
-        app.salutare('Alberto').end((err, res) => {
+        app.salutare('Alberto').end((res) => {
             res.should.equal.to('Hello World');
         })
     })
